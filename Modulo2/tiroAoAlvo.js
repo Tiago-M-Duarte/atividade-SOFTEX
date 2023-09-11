952f64e6817cfd2315a47e81ee260ac3fa213a4b
Tiago-M-Duarte <tiagomduartepro@gmail.com>
2023-09-11 15:41:02 -0300

    tiroAoAlvo.js

---
+++ Modulo2/tiroAoAlvo.js
@@ -0,0 +1,7 @@
+const participantes = [
+    {nome: 'João', pontos: 50},
+    {nome: 'Pedro', pontos: 120},
+    {nome: 'Luiza', pontos: 70},
+    {nome: 'Marcela', pontos: 10},
+    {nome: 'Yuri', pontos: 34}
+]
